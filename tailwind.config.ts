db74f97584b0407b871fda79d95d92d149b496bc
RezaFarzipour <rezafafa78@gmail.com>
2025-03-11 13:13:55 +0330

    divider installed on package json

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@heroui/theme/dist/components/(button|input-otp|ripple|spinner|form).js"
+    "./node_modules/@heroui/theme/dist/components/(button|divider|input-otp|ripple|spinner|form).js"
   ],
   theme: {
     extend: {
