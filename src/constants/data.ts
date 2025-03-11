49d6becf60d0e97d739fc6bb065b4f30d08d7531
RezaFarzipour <rezafafa78@gmail.com>
2025-03-11 13:34:23 +0330

    1-footer created 2-data file created in constants

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -1,1 +1,33 @@
-const initial="initial"
+import { TiHomeOutline } from "react-icons/ti";
+import { CiWallet } from "react-icons/ci";
+import { IoMdExit } from "react-icons/io";
+
+type Data = {
+    id:number,
+  title: string;
+  icon: React.ElementType;
+  hover: string;
+};
+
+export const sidebarlinks: Data[] = [
+  {
+    id:0,
+    title: "داشبورد",
+    icon: TiHomeOutline,
+    hover: "text-primary-500",
+  },
+
+  {
+    id:1,
+    title: "سفارش های من",
+    icon: CiWallet,
+    hover: "text-primary-500",
+  },
+
+  {
+    id:2,
+    title: "خروج",
+    icon: IoMdExit,
+    hover: "text-danger-500",
+  },
+];
