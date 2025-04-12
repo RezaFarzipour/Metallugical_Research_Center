53a393663c89d3758e376b44f2a68c1977ec083b
RezaFarzipour <rezafafa78@gmail.com>
2025-04-12 16:36:28 +0330

    userAddEditPage created in template CustomInput element created and added to userAddEditPage user userAddEditPage added in adduser page

---
+++ src/app/(panel)/admin/adduser/page.tsx
@@ -0,0 +1,10 @@
+import UserEditPage from '@/components/template/adminPanel/userAddEditPage'
+import React from 'react'
+
+const page = () => {
+  return (
+  <UserEditPage/>
+  )
+}
+
+export default page
