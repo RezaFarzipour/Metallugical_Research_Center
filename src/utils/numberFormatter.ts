3fcf9e34ebafa4677a28ce428e653781f4f0d5e1
RezaFarzipour <rezafafa78@gmail.com>
2025-04-12 16:40:39 +0330

    number seprator function addded to numberFormatter

--- src/utils/numberFormatter.ts
+++ src/utils/numberFormatter.ts
@@ -2,3 +2,13 @@ export function toPersianDigits(n: number | string): string {
   const farsiDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
   return n.toString().replace(/\d/g, (x) => farsiDigits[parseInt(x, 10)]);
 }
+
+
+export const sp = (number:string |number ) => {
+  const seperatedNumber = number
+    .toString()
+    .match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g);
+    
+  const joinedNumber = seperatedNumber?.join(",");
+  return (joinedNumber);
+};
