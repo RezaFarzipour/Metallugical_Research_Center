d30740a5bbd39ac51791a1d40dd2b857aaca31c1
RezaFarzipour <rezafafa78@gmail.com>
2025-04-12 16:33:09 +0330

    install :accrodian from hero ut create:accordian in FaqAccordian add:add faq in landing page type:add faq types in type file

--- src/components/template/landingPage.tsx
+++ src/components/template/landingPage.tsx
@@ -2,13 +2,15 @@
 import React from "react";
 import AboutOverview from "../module/AboutOverview";
 import BlogCardModule from "../module/BlogCardModule";
+import FaqAccordian from "../module/FaqAccordian";
 type Props = {};
 
 const LandingPage = (props: Props) => {
   return (
     <>
       <AboutOverview />
-      <BlogCardModule />.
+      <BlogCardModule />
+      <FaqAccordian/>
     </>
   );
 };
