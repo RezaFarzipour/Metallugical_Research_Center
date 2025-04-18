78ba9cd0158643bf35c6b286d956ce4661985454
emad <arabanemad@gmail.com>
2025-04-18 20:34:17 +0330

    add blogs page

---
+++ src/app/blogs/layout.tsx
@@ -0,0 +1,22 @@
+import Layout from "@/components/containers/layout/Layout";
+import HeaderImage from "@/components/module/HeaderImage";
+
+export default function ContactUsLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <Layout>
+        {/* banner section */}
+        <HeaderImage
+          headrName="بلاگ ها "
+          breadcrumbItem2="بلاگ ها"
+          panelHref="/aboutus"
+        />
+        {children}
+      </Layout>
+    </>
+  );
+}
---
+++ src/app/blogs/page.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  //   title: "Bolgs",
+  title: {
+    absolute: "blogs", // ignore %s in parent layout.
+  },
+};
+
+function BlogsPage() {
+  return <div>بلاگ ها </div>;
+}
+export default BlogsPage;
