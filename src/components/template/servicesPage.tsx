335610305a3993d146b53ab23a6edc4d9d6f6e01
emad <arabanemad@gmail.com>
2025-04-26 00:25:48 +0330

    update services

--- src/components/template/servicesPage.tsx
+++ src/components/template/servicesPage.tsx
@@ -4,14 +4,22 @@ import React from "react";
 import TitleStructure from "../element/TitleStructure";
 import SectionWrapper from "@/hoc/SectionWrapper";
 import FilteredContainer from "../containers/FilteredContainer";
-import { products } from "@/constants/tableData";
 import { useTableStore } from "@/store/useTableSlice";
 import { useFilteredContainer } from "@/hooks/useFilteredContainer";
 import CardModule from "../module/cardModule/CardModule";
+import { useQuery } from "@tanstack/react-query";
+import { getAllServiceAdmin } from "@/services/service";
 
 const Services: React.FC = () => {
   const { view } = useTableStore();
-  const { sortedItems } = useFilteredContainer(products);
+  const { data, isPending } = useQuery({
+    queryKey: ["getAll-services"],
+    queryFn: getAllServiceAdmin,
+  });
+
+  // گرفتن آرایه‌ی سرویس‌ها از formData
+  const formDataServices = Array.isArray(data) ? data : [];
+  const { sortedItems } = useFilteredContainer(formDataServices);
 
   return (
     <div className="container flex flex-col items-center justify-center gap-5 max-w-screen-lg pt-72">
@@ -21,22 +29,24 @@ const Services: React.FC = () => {
 
       <div className="flex flex-col gap-36 lg:gap-5 lg:flex-row justify-center w-full items-center p-4">
         <FilteredContainer
-          users={products}
-          quantity="خدمات  ما "
-          viewContent
-          viewContentSmSize
-          topContents
-          bottomContents
+          datas={formDataServices}
+          quantity="سرویس ها "
+          topContents={true}
+          viewContent={true}
+          viewContentSmSize={false}
           btn={false}
           dropDownBtn={false}
           roles={false}
-          product={false}
+          addBtn={false}
+          rolesDropDown={false}
+          stausDropDown={false}
+          bottomContents={true}
         >
           <div
             className={`grid grid-cols-1 ${
               view ? "sm:grid-cols-2 lg:grid-cols-3" : ""
             } gap-4 mt-10 mb-32`}
-            style={{ width: "1200px" }} // Fixed width added here
+            style={{ width: "1200px" }}
           >
             <CardModule
               data={sortedItems}
