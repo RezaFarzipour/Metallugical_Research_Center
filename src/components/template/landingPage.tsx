d6f4ee9b77decafd7e6229ef884a84df7e7742a4
RezaFarzipour <rezafafa78@gmail.com>
2025-04-30 15:31:05 +0330

    remove button from landing

--- src/components/template/landingPage.tsx
+++ src/components/template/landingPage.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import BlogCardModule from "../module/BlogCardModule";
 import FaqAccordian from "../module/FaqAccordian";
 import LandingHeader from "../module/LandingHeader";
-import { showToast } from "@/store/useToastSlice";
 import AboutOverview from "../module/aboutOverview/AboutOverview";
 
 const LandingPage = () => {
@@ -28,9 +27,7 @@ const LandingPage = () => {
       <section className="w-full max-w-7xl mx-auto px-4 md:px-8 py-16">
         <BlogCardModule />
       </section>
-      <button onClick={() => showToast("عملیات موفقیت‌آمیز بود", "success")}>
-        click
-      </button>
+     
     </main>
   );
 };
