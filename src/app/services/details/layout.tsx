2bf25c3388e2cc37b8cf2cbcb7b8b61b643b0c69
RezaFarzipour <rezafafa78@gmail.com>
2025-05-01 17:08:19 +0330

    services detail layout deleted btnLoader added to servicesPage

--- src/app/services/details/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from "@/components/containers/layout/Layout";
-
-export default function DetailsLayout({ children }: { children: React.ReactNode }) {
-   return (
-    <Layout>
-        {children}
-    </Layout>
-   )
-  }
