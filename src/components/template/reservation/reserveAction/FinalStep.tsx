beabc11e0e4a6c2c5798512d1cf7696a7053668b
RezaFarzipour <rezafafa78@gmail.com>
2025-05-12 12:42:04 +0330

    finalstep in reservation updated ui v1

--- src/components/template/reservation/reserveAction/FinalStep.tsx
+++ src/components/template/reservation/reserveAction/FinalStep.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { reservationDataType, ServiceDetailsType } from "@/types";
 import Image from "next/image";
+import { FaCheck } from "react-icons/fa";
+import { sp } from "@/utils/formatter/numberFormatter";
+import { formatDateRangesToPersian } from "@/utils/formatter/formatDateRangesToPersian";
 
 type Stage6Props = {
   data: reservationDataType;
@@ -9,40 +12,37 @@ type Stage6Props = {
 
 const FinalStep = ({ data, serviceData }: Stage6Props) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[60vh] space-y-6 text-center p-6">
-      <h2 className="text-2xl font-bold text-green-600">
-        رزرو با موفقیت انجام شد!
-      </h2>
+    <div className="w-full container rounded-xl h-auto flex-col  flex justify-center items-center bg-white p-4 [box-shadow:rgba(100,100,111,0.2)_0px_7px_29px_0px]">
+      <div className="p-6 rounded-full bg-[#DCFCE7]">
+        <FaCheck className="text-green-600" size={"25px"} />
+      </div>
+      <h2 className="text-xl font-extrabold my-4">رزرو با موفقیت انجام شد</h2>
 
-      <div className="p-4 border rounded shadow-md max-w-md w-full space-y-4 bg-white">
-        {serviceData?.data?.cover_image ? (
-          <Image
-            src={serviceData.data.cover_image}
-            width={100}
-            height={100}
-            alt="سرویس"
-            className="w-32 h-32 object-cover mx-auto rounded"
-          />
-        ) : (
-          <div className="w-32 h-32 bg-gray-200 flex items-center justify-center text-sm text-gray-500 mx-auto rounded">
-            بدون تصویر
+
+<div className="bg-[#F0FDF4] p-6 rounded-md max-w-xl mx-auto">
+        <h2 className="text-lg font-bold mb-4 text-center">اطلاعات رزرو</h2>
+        <div className="grid grid-cols-2 gap-y-3 text-sm">
+          <div className="font-medium">کاربر</div>
+          <div>{data.user || "نامشخص"}</div>
+
+          <div className="font-medium">نام سرویس</div>
+          <div>{serviceData?.data.service_name || "نامشخص"}</div>
+
+          <div className="font-medium">توضیحات سرویس</div>
+          <div>{serviceData?.data.description || "نامشخص"}</div>
+
+          <div className="font-medium"> قیمت</div>
+          <div>{sp(serviceData?.data.price) || "نامشخص"}</div>
+
+          <div className="font-medium">تاریخ رزرو</div>
+          <div>
+            {formatDateRangesToPersian([
+              {
+                reserved_from: data.reserve_from || "",
+                reserved_to: data.reserve_to || "",
+              },
+            ]) || "?"}
           </div>
-        )}
-
-        <div className="space-y-1 text-right">
-          <p>
-            <strong>نام سرویس:</strong> {serviceData?.data?.service_name}
-          </p>
-          <p>
-            <strong>قیمت کل:</strong> {data?.total_price?.toLocaleString()}{" "}
-            تومان
-          </p>
-          <p>
-            <strong>مدت زمان رزرو:</strong> {data?.reserve_duration} دقیقه
-          </p>
-          <p>
-            <strong>توضیحات ادمین:</strong> {data?.admin_description || "—"}
-          </p>
         </div>
       </div>
     </div>
