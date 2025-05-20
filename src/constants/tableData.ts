7621f33b78d51048f266e82d1a46ce8c5ed89b55
emad <arabanemad@gmail.com>
2025-05-20 12:23:39 +0330

    update tableData

--- src/constants/tableData.ts
+++ src/constants/tableData.ts
@@ -1,130 +1,30 @@
-export const usersOrders = [
-  {
-    id: 1,
-    name: "Tony Reichert",
-    amount: "1000",
-    status: "انجام شده",
-    date: "1403/01/22",
-    email: "tony.reichert@example.com",
-  },
-  {
-    id: 2,
-    name: "Zoey Lang",
-    amount: "1000",
-    status: "کنسل شده",
-    date: "1403/01/22",
-    email: "zoey.lang@example.com",
-  },
-  {
-    id: 3,
-    name: "Jane Fisher",
-    amount: "1000",
-    status: "انجام شده",
-    date: "1403/01/22",
-    email: "jane.fisher@example.com",
-  },
-  {
-    id: 4,
-    name: "William Howard",
-    amount: "1000",
-    status: "درحال انتظار",
-    date: "1403/01/22",
-    email: "william.howard@example.com",
-  },
-  {
-    id: 5,
-    name: "علی محمدی",
-    amount: "1000",
-    status: "کنسل شده",
-    date: "1403/01/22",
-    email: "kristen.cooper@example.com",
-  },
-  {
-    id: 6,
-    name: "Michael Smith",
-    amount: "1200",
-    status: "انجام شده",
-    date: "1403/01/22",
-    email: "michael.smith@example.com",
-  },
-  {
-    id: 7,
-    name: "Emily Johnson",
-    amount: "1500",
-    status: "درحال انتظار",
-    date: "1403/01/22",
-    email: "emily.johnson@example.com",
-  },
-  {
-    id: 8,
-    name: "David Brown",
-    amount: "800",
-    status: "کنسل شده",
-    date: "1403/01/22",
-    email: "david.brown@example.com",
-  },
-  {
-    id: 9,
-    name: "Sophia Davis",
-    amount: "900",
-    status: "انجام شده",
-    date: "1403/01/22",
-    email: "sophia.davis@example.com",
-  },
-  {
-    id: 10,
-    name: "James Wilson",
-    amount: "1100",
-    status: "درحال انتظار",
-    date: "1403/01/22",
-    email: "james.wilson@example.com",
-  },
-  {
-    id: 11,
-    name: "Isabella Martinez",
-    amount: "1300",
-    status: "کنسل شده",
-    date: "1403/01/22",
-    email: "isabella.martinez@example.com",
-  },
-];
-export const usersOrdersINITIAL_VISIBLE_COLUMNS = [
-  "id",
-  "name",
-  "status",
-  "date",
-  "amount",
-  "actions",
-];
-
-
 
 export const Usercolumns = [
-  { name: "#", uid: "id", sortable: true },
-  { name: "نام و نام خانوادگی", uid: "name", sortable: true },
-  { name: "نقش", uid: "role", sortable: true },
+  { name: "#", uid: "id" },
+  { name: "نام و نام خانوادگی", uid: "name" },
+  { name: "نقش", uid: "role" },
   { name: "ایمیل", uid: "email" },
   { name: "شماره همراه", uid: "phone_number" },
   { name: "عملیات", uid: "actions" },
 ];
 
 export const Servicecolumns = [
-  { name: "#", uid: "_id", sortable: true },
-  { name: "نام سرویس", uid: "name", sortable: true },
-  { name: "توضیحات", uid: "description", sortable: true },
+  { name: "#", uid: "_id" },
+  { name: "نام سرویس", uid: "name" },
+  { name: "توضیحات", uid: "description" },
   { name: "قیمت", uid: "price" },
   { name: "تاریخ باز", uid: "dateRange" },
   { name: "عملیات", uid: "actions" },
 ];
 
 export const ReservesAdmincolumns = [
-  { name: "#", uid: "_id", sortable: true },
-  { name: "نام کاربر", uid: "name", sortable: true },
-  { name: "شماره همراه", uid: "phone_number", sortable: true },
-  { name: "نام سرویس", uid: "service_name", sortable: true },
+  { name: "#", uid: "_id" },
+  { name: "نام کاربر", uid: "name" },
+  { name: "شماره همراه", uid: "phone_number" },
+  { name: "نام سرویس", uid: "service_name" },
   { name: "قیمت", uid: "price" },
   { name: "مدت زمان رزرو", uid: "reserve_duration" },
-  { name: "توضیحات", uid: "admin_description", sortable: true },
+  { name: "توضیحات", uid: "admin_description" },
   { name: "تاریخ رزرو", uid: "dateRange" },
   { name: " وضعیت پرداخت", uid: "payment_status" },
   { name: "وضعیت", uid: "status" },
@@ -132,13 +32,13 @@ export const ReservesAdmincolumns = [
   { name: "عملیات", uid: "actions" },
 ];
 export const ReservesCustomercolumns = [
-  { name: "#", uid: "_id", sortable: true },
-  { name: "نام کاربر", uid: "name", sortable: true },
-  { name: "نام سرویس", uid: "service_name", sortable: true },
+  { name: "#", uid: "_id" },
+  { name: "نام کاربر", uid: "name" },
+  { name: "نام سرویس", uid: "service_name" },
   { name: "قیمت", uid: "price" },
   { name: "مدت زمان رزرو", uid: "reserve_duration" },
 
-  { name: "توضیحات", uid: "admin_description", sortable: true },
+  { name: "توضیحات", uid: "admin_description" },
   { name: "تاریخ رزرو", uid: "dateRange" },
 
   { name: " وضعیت پرداخت", uid: "payment_status" },
@@ -149,13 +49,13 @@ export const ReservesCustomercolumns = [
 ];
 
 export const ReportsCustomercolumns = [
-  { name: "#", uid: "_id", sortable: true },
-  { name: "نام کاربر", uid: "name", sortable: true },
-  { name: "نام سرویس", uid: "service_name", sortable: true },
+  { name: "#", uid: "_id" },
+  { name: "نام کاربر", uid: "name" },
+  { name: "نام سرویس", uid: "service_name" },
   { name: "قیمت", uid: "price" },
   { name: "مدت زمان رزرو", uid: "reserve_duration" },
 
-  { name: "توضیحات", uid: "admin_description", sortable: true },
+  { name: "توضیحات", uid: "admin_description" },
   { name: "تاریخ رزرو", uid: "dateRange" },
 
   { name: " وضعیت پرداخت", uid: "payment_status" },
@@ -165,23 +65,25 @@ export const ReportsCustomercolumns = [
 ];
 
 export const columns = [
-  { name: "#", uid: "id", sortable: true },
-  { name: "نام", uid: "full-name", sortable: true },
-  { name: "نقش", uid: "role", sortable: true },
+  { name: "#", uid: "id" },
+  { name: "نام", uid: "full-name" },
+  { name: "نقش", uid: "role" },
   { name: "ایمیل", uid: "email" },
-  { name: "وضعیت", uid: "status", sortable: true },
-  { name: "تاریخ", uid: "date", sortable: true },
-  { name: "قیمت", uid: "amount", sortable: true },
+  { name: "وضعیت", uid: "status" },
+  { name: "تاریخ", uid: "date" },
+  { name: "قیمت", uid: "amount" },
   { name: "عملیات", uid: "actions" },
 ];
 
 export const blogColumns = [
-  { name: "#", uid: "id", sortable: true },
-  { name: "عنوان", uid: "title", sortable: true },
-  { name: "دسته بندی", uid: "category", sortable: true },
+  { name: "#", uid: "id" },
+  { name: "عنوان", uid: "title" },
+  { name: "دسته بندی", uid: "category" },
   { name: "عملیات", uid: "actions" },
-  { name: "تگ ها", uid: "tags", sortable: true },
+  { name: "تگ ها", uid: "tags" },
 ];
+
+
 
 export const statusOptions = [
   { name: "انجام شده", uid: "انجام شده" },
@@ -200,7 +102,7 @@ export const rolesOptions = [
 
 export const statusColorMap = {
   "تمام شده": "success",
-  "درحال انتظار": "warning",
+  "در حال انتظار": "warning",
   "لغو شده": "danger",
   "پرداخت شده": "success",
   "در انتظار پرداخت": "warning",
