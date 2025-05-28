bf6ceab696068c581d61b31d13815cee4ef05a9b
emad <arabanemad@gmail.com>
2025-05-28 14:58:41 +0330

    create Button Custome

--- src/components/element/Button.tsx
+++ src/components/element/Button.tsx
@@ -1,10 +1,13 @@
+import { cn } from "@/utils/cn";
+import { useRouter } from "next/navigation";
 import { ButtonHTMLAttributes, ReactNode } from "react";
-import { twMerge } from "tailwind-merge";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
-  variant?: "primary" | "secondary";
+  variant?: "primary" | "secondary" | "danger" | "warning" | "success";
   fullWidth?: boolean;
+  type?: "submit" | "reset" | "button" | undefined;
+  path?: string | undefined;
 }
 
 const Button = ({
@@ -12,26 +15,40 @@ const Button = ({
   className,
   variant = "primary",
   fullWidth = false,
+  type = "button",
+  path,
   ...props
 }: ButtonProps) => {
-  const baseStyles = "px-4 py-2 rounded-lg font-medium transition-colors";
+  const router = useRouter();
+  const baseStyles =
+    "relative inline-flex items-center justify-center px-6 py-2 overflow-hidden font-semibold transition-all duration-300 ease-in-out rounded-md shadow-lg group";
 
   const variants = {
-    primary: "bg-primary-500 text-primary-50 hover:bg-primary-600",
-    secondary: "bg-secondary-500 text-primary-50 hover:bg-secondary-600",
+    primary:
+      "text-white bg-gradient-to-r from-secondary-500 to-secondary-700 hover:from-secondary-600 hover:to-secondary-800",
+    secondary:
+      "text-white bg-gradient-to-r from-gray-500 to-gray-700 hover:from-gray-600 hover:to-gray-800",
+    danger:
+      "text-white bg-gradient-to-r from-red-500 to-red-700 hover:from-red-600 hover:to-red-800",
+    warning:
+      "text-white bg-gradient-to-r from-yellow-500 to-yellow-700 hover:from-yellow-600 hover:to-yellow-800",
+    success:
+      "text-white bg-gradient-to-r from-green-500 to-green-700 hover:from-green-600 hover:to-green-800",
   };
 
   return (
     <button
-      className={twMerge(
+      className={cn(
         baseStyles,
         variants[variant],
         fullWidth && "w-full",
         className
       )}
+      type={type}
       {...props}
+      onClick={() => router && router.push(path)}
     >
-      {children}
+      <span className="relative z-10">{children}</span>
     </button>
   );
 };
