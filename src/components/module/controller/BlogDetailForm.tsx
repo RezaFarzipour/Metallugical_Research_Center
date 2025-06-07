ec5529ea5e6f6f32c36f2824a2beac952739607b
RezaFarzipour <rezafafa78@gmail.com>
2025-06-07 13:11:52 +0330

    BlogDetailForm created

---
+++ src/components/module/controller/BlogDetailForm.tsx
@@ -0,0 +1,56 @@
+import RHFInput from "@/components/element/RHFInput";
+import { BlogStageOneFormData } from "@/schemas/blogStageOneSchema";
+import { CreateServiceFormData } from "@/schemas/createServiceSchema";
+import React from "react";
+import { UseFormRegister, FieldErrors } from "react-hook-form";
+
+interface ServiceDetailsFormProps {
+  register: UseFormRegister<BlogStageOneFormData>;
+  errors: FieldErrors<BlogStageOneFormData>;
+}
+
+const BlogDetailsForm: React.FC<ServiceDetailsFormProps> = ({
+  register,
+  errors,
+}) => {
+  return (
+    <div>
+      <RHFInput<BlogStageOneFormData>
+        register={register}
+        errors={errors}
+        label="نام بلاگ"
+        type="text"
+        dir="rtl"
+        name="title"
+      />
+
+      <RHFInput<BlogStageOneFormData>
+        register={register}
+        errors={errors}
+        label="تگ ها"
+        type="text"
+        dir="rtl"
+        name="tags"
+      />
+
+      <RHFInput<BlogStageOneFormData>
+        register={register}
+        errors={errors}
+        label="اسلاک"
+        type="text"
+        dir="rtl"
+        name="slug"
+      />
+       <RHFInput<BlogStageOneFormData>
+        register={register}
+        errors={errors}
+        label="اسلاک"
+        type="text"
+        dir="rtl"
+        name="slug"
+      />
+    </div>
+  );
+};
+
+export default BlogDetailsForm;
