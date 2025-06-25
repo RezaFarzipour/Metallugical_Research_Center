4f8d523437cf2a8cbffa649e118bfe8cd0c1532f
emad <arabanemad@gmail.com>
2025-06-25 14:45:02 +0330

    update warningPage to expiredReservePage

---
+++ src/app/(admin-panel)/admin/expiredReserve/page.tsx
@@ -0,0 +1,12 @@
+import ExpiredReservePage from "@/components/template/adminPanel/expiredReserve/expiredReservePage";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <ExpiredReservePage />
+    </div>
+  );
+};
+
+export default page;
