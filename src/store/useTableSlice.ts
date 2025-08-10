7f9ec04a49f7d35a0dac207c8eb79fe6288e0e6e
emad <arabanemad@gmail.com>
2025-08-11 01:02:18 +0330

    add createTableStore

--- src/store/useTableSlice.ts
+++ src/store/useTableSlice.ts
@@ -50,3 +50,38 @@ export const useTableStore = create<TableState>((set) => ({
     setPage: (page: number) => set({ page }),
     setView: (view: boolean) => set({ view }),
 }));
+
+
+export const createTableStore = () =>
+    create<TableState>((set) => ({
+        filterValue: "",
+        selectedKeys: new Set<string>(),
+        visibleColumns: new Set<string>(),
+        statusFilter: "all",
+        peymentStatusFilter: "all",
+        rolesFilter: "all",
+        rowsPerPage: 5,
+        sortDescriptor: { column: "id", direction: "ascending" },
+        page: 1,
+        view: false,
+
+        setFilterValue: (value) => set({ filterValue: value }),
+        setSelectedKeys: (keys) => set({ selectedKeys: keys }),
+        setVisibleColumns: (columns) => set({ visibleColumns: columns }),
+        setStatusFilter: (filter) => set({ statusFilter: filter }),
+        setPeymentStatusFilter: (filter) => set({ peymentStatusFilter: filter }),
+        setRolesFilter: (filter) => set({ rolesFilter: filter }),
+        setRowsPerPage: (rows) => set({ rowsPerPage: rows }),
+        setSortDescriptor: (descriptor) => set({ sortDescriptor: descriptor }),
+        setPage: (page) => set({ page }),
+        setView: (view) => set({ view }),
+    }));
+
+
+
+export const useReportsTableStore = createTableStore();
+export const useReservesTableStore = createTableStore();
+export const useUsersTableStore = createTableStore();
+export const useCoursesTableStore = createTableStore();
+export const useBlogsTableStore = createTableStore();
+export const useAdminServicesTableStore = createTableStore();
